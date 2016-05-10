e8df8ad97aa53d7828f7a7c4ddc6a1cc7fca99bb
Jijeshp920 <jijeshp920@gmail.com>
2016-05-10 21:51:12 +0400

    Update StatusbarTransparent.java

--- src/android/StatusbarTransparent.java
+++ src/android/StatusbarTransparent.java
@@ -9,16 +9,31 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.view.WindowManager.LayoutParams;
 
+import android.os.Build;
+import android.util.Log;
+import android.view.Window;
+import android.graphics.Color;
+import android.view.View;
+
 public class StatusbarTransparent extends CordovaPlugin {
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
 		// grab the correct methods
 		if(action.equalsIgnoreCase("enable")) {
-			if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
+			if(VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
 				cordova.getActivity().runOnUiThread( new Runnable() {
+					
+					
 					public void run() {
-						cordova.getActivity().getWindow().addFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
+						
+		cordova.getActivity().getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        //Status bar color, set to whatever opacity/color you want
+         
+        			cordova.getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+						
+						
+					//	cordova.getActivity().getWindow().addFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
 					}
 				});
 				callback.success();
