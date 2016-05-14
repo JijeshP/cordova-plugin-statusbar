afdd04b17ece7eb6b34ab26dd1c705ae80af6bc0
Jijeshp920 <jijeshp920@gmail.com>
2016-05-14 17:52:58 +0400

    Update StatusbarTransparent.java

--- src/android/StatusbarTransparent.java
+++ src/android/StatusbarTransparent.java
@@ -15,47 +15,25 @@ import android.view.Window;
 import android.graphics.Color;
 import android.view.View;
 
+
+	import org.apache.cordova.CordovaInterface;
+ import org.apache.cordova.CordovaWebView;
+
 public class StatusbarTransparent extends CordovaPlugin {
 
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
-		// grab the correct methods
-		if(action.equalsIgnoreCase("enable")) {
-			if(VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-				cordova.getActivity().runOnUiThread( new Runnable() {
-					
-					
-					public void run() {
-						
-		cordova.getActivity().getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        //Status bar color, set to whatever opacity/color you want
-         
-        			cordova.getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
-						
-						
-					//	cordova.getActivity().getWindow().addFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
-					}
-				});
-				callback.success();
-			} else {
-				callback.error("not supported");
-			}
-			return true;
-		} else if(action.equalsIgnoreCase("disable")) {
-			if(VERSION.SDK_INT >= VERSION_CODES.KITKAT) {
-				cordova.getActivity().runOnUiThread( new Runnable() {
-					public void run() {
-						cordova.getActivity().getWindow().clearFlags(LayoutParams.FLAG_TRANSLUCENT_STATUS);
-					}
-				});
-				callback.success();
-			} else {
-				callback.error("not supported");
-			}
-			return true;
-		} else {
-			callback.error("Unknown Action: " + action);
-			return false;
-		}
-	}
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+           
+cordova.getActivity().getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+    //Status bar color, set to whatever opacity/color you want
+
+    cordova.getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+                
+        }
+    }
 }
+               
+		
+	 
